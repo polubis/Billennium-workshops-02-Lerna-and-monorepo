f23dedc562e43a1c548a90c4f115f592c47ebd28
Adrian Polubinski <adrian.polubinski@contractors.roche.com>
2021-08-02 16:54:59 +0200

    2. Create schema validation library

---
+++ libs/schema/src/core.ts
@@ -0,0 +1,45 @@
+export type Validator<T> = (value: T) => boolean;
+export type Validators<T> = Validator<T>[];
+export type Result = Record<string, boolean>;
+
+export const createValidator = <T>(
+  validator: Validator<T>,
+  name: string
+): Validator<T> => {
+  const namedValidator = Object.defineProperty(validator, "name", {
+    value: name,
+    configurable: true,
+  });
+
+  return namedValidator;
+};
+
+export const required = createValidator<string>(
+  (value) => value === "",
+  "required"
+);
+
+export const minLength = (limit: number) =>
+  createValidator<string | unknown[]>(
+    (value) => value.length < limit,
+    "minLength"
+  );
+
+export const maxLength = (limit: number) =>
+  createValidator<string | unknown[]>(
+    (value) => value.length > limit,
+    "maxLength"
+  );
+
+export const Schema = <T>(value: T, validators: Validators<T>) => {
+  return {
+    result: () =>
+      validators.reduce<Result>((acc, validator) => {
+        // Workaround
+        // Invalid type declaration inside lib.d.ts with global JS typings
+        acc[(validator as any).name] = validator(value);
+        return acc;
+      }, {}),
+    valid: (): boolean => validators.some((validator) => validator(value)),
+  };
+};
