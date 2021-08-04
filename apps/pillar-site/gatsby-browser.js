ffa77ae424caa30cb825e8a8408bbca9833260bb
Adrian Polubinski <adrian.polubinski@contractors.roche.com>
2021-08-04 17:37:53 +0200

    11. Add gatsby project site

---
+++ apps/pillar-site/gatsby-browser.js
@@ -0,0 +1,16 @@
+import "firebase/firebase-firestore";
+
+import "./src/styles/index.scss"
+
+import "@fontsource/roboto/300.css"
+import "@fontsource/roboto/400.css"
+import "@fontsource/roboto/500.css"
+import "@fontsource/roboto/700.css"
+
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/browser-apis/
+ */
+
+// You can delete this file if you're not using it
---
+++ apps/pillar-site/gatsby-config.js
@@ -0,0 +1,50 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+ require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` })
+
+ module.exports = {
+   /* Your site config here */
+   plugins: [
+     'gatsby-plugin-sass',
+     {
+       resolve: `gatsby-plugin-env-variables`,
+       options: {
+         allowList: [],
+       },
+     },
+     {
+       resolve: "gatsby-plugin-firebase",
+       options: {
+         credentials: {
+           apiKey: process.env.API_KEY,
+           authDomain: process.env.AUTH_DOMAIN,
+           projectId: process.env.PROJECT_ID,
+           storageBucket: process.env.STORAGE_BUCKET,
+           messagingSenderId: process.env.MESSAGING_SENDER_ID,
+           appId: process.env.APP_ID,
+           measurementId: process.env.MEASURMENT_ID,
+         },
+       },
+     },
+ 
+     "gatsby-plugin-styled-components",
+     "gatsby-plugin-sass",
+     {
+       resolve: `gatsby-plugin-webfonts`,
+       options: {
+         fonts: {
+           google: [
+             {
+               family: "Roboto",
+               variants: ["400", "500", "700"],
+             },
+             { family: "Open Sans" },
+           ],
+         },
+       },
+     },
+   ],
+ }
